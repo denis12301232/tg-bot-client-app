ea33bdff2afe2db5af4c36c38f43a49627c11b16
denis12301232 <test12301232@gmail.com>
2023-06-07 12:43:39 +0300

    multilang for error page

--- src/components/TheError.vue
+++ src/components/TheError.vue
@@ -2,13 +2,18 @@
   <div class="window-height column items-center justify-center">
     <h1 class="q-py-md text-negative text-uppercase">{{ code }}</h1>
     <h3 class="q-py-md text-uppercase">{{ message }}</h3>
-    <QBtn class="q-mt-lg" color="primary" label="На главную" @click="$router.push('/')" />
+    <QBtn class="q-mt-lg" color="primary" :label="t('error.btn')" @click="$router.push('/')" />
   </div>
 </template>
 
 <script setup lang="ts">
+import type { I18n, Langs } from '@/types';
+import { useI18n } from 'vue-i18n';
+
 defineProps<{
   code: number;
   message: string;
 }>();
+
+const { t } = useI18n<I18n, Langs>();
 </script>
